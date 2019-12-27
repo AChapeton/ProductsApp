4f7a2f4932124f1e37ff75f1ee0f05efb4480bff
Andres Chapeton <andres.chapeton0206@gmail.com>
2019-12-26 19:47:32 -0600

    Initian commit. Add To-Do

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  state = {
+    message: "ToDo App",
+    newTodo: "",
+    toDos: [
+      {
+        title: "Learn React",
+        done: false
+      },
+      {
+        title: "Learn Vue",
+        done: false
+      }
+    ]
+  }  
+
+  HandleChange(e){
+    this.setState({
+      newTodo: e.target.value
+    })
+  }
+
+  HandleSubmit(e){
+    e.preventDefault()
+    this.setState({
+      toDos: [...this.state.toDos, {
+        title: this.state.newTodo,
+        done: false
+      }]
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1>{this.state.message}</h1>
+        <form onSubmit={(e) => this.HandleSubmit(e)}> 
+          <input onChange={(e) => this.HandleChange(e)} type="text" placeholder="Add ToDo" value={this.state.newTodo}/>
+          <button type="submit" className="btn btn-secondary">Add</button>
+        </form>
+        <ul>
+          {
+            this.state.toDos.map(todo => {
+              return (
+                <li key={todo.title}>
+                  <input type="checkbox"/> {todo.title}
+                </li>)
+            })
+          }
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
